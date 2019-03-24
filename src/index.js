3b02f8a7314335b5be4eecc128f810b3c1101db0
alexanderrabetsky <3820168Rabetsky>
2019-03-24 22:12:41 +0300

    init

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0;
+    let membersInTriangle = [];
+    preferences.forEach((elem, index, arr) => {
+        if (!membersInTriangle.includes(index)) {
+            let first = elem;
+            let second = arr[first-1];
+            let third = arr[second-1];
+            if (index == third-1
+                && first != second
+                && second != third
+            ) {
+                ++count;
+                membersInTriangle.push(index, first-1, second-1);
+            }
+        }
+    });
+    return count;
 };
+
